86cab56478acf0587dcaa1f4728e800b98a25f29
hengkanglengkang <49785411+hengkanglengkang@users.noreply.github.com>
2019-04-19 19:49:34 +0700

    memulia membuat file

---
+++ latihan_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class exercise
+{
+public:
+  exercise(){
+    cout<<"hello world"<<endl;
+  }
+}
+
+int main()
+{
+  exercise first;
+  _getch();
+  return 0;
+}
